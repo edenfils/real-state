caac9c76382e677920cf3400a34d84346524f6e5
Eden Fils <ednfils@gmail.com>
2019-04-20 17:33:35 -0400

    creating the footer component

---
+++ assets/js/realestate/includes/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    
+  }
+}
