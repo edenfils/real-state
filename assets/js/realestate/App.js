7d31b49ce38d352813b6706652a50541dd840e34
Eden Fils <ednfils@gmail.com>
2019-04-21 16:51:32 -0400

    setting up listings route

--- assets/js/realestate/App.js
+++ assets/js/realestate/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Header from './includes/Header.js';
 import Footer from './includes/Footer.js';
 import Home from './pages/Home.js';
+import Listings from './pages/Listings.js';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default class App extends Component {
@@ -19,6 +20,7 @@ export default class App extends Component {
 				<div>
 					<Header />
 					<Route exact path="/" component={Home} />
+					<Route exact path="/listings" component={Listings} />
 					<Footer />
 				</div>
 			</Router>
--- assets/js/realestate/pages/Listings.js
+++ assets/js/realestate/pages/Listings.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Listings extends Component {
-  constructor() {
-    super();
-    this.state = {}
-  }
+	constructor() {
+		super();
+		this.state = {};
+	}
 
-  render() {
-    return {(
-      <div>Listings page</div>
-    )}
-  }
+	render() {
+		return <div>Listings page</div>;
+	}
 }
