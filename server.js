b9bd854c824e8666aebf2c91c418e1675906d32f
Eden Fils <ednfils@gmail.com>
2019-04-24 18:20:02 -0400

    setting up api for listings

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const serveStatic = require('serve-static');
 
 const path = require('path');
 
+//data
+
+const listingsData = require('./data/listingsData.js');
+
 const app = express();
 
 app.use('/', serveStatic(path.join(__dirname, '/public')));
+
+app.use('/api/listings', function(req, res) {
+  res.json(listingsData);
+})
 
 app.get('*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html')
